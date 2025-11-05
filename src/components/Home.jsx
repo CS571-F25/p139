b0461e528ea44c8f9ef33b50796da68e8c038a27
jho33 <jho33@wisc.edu>
2025-11-05 12:18:18 -0600

    updated Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 export default function Home () {
     return <div>
-        <h1>Home page</h1>
+        <h1>Travel Tracker</h1>
+        <p>Travel Tracker is a web project that presents my travel journal through an interactive world map, designed to encourage playful exploration while staying lightweight and accessible.</p>
     </div>
 }
